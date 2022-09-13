22f3a52e95f3f619b25a43d42a026ed3a05514ee
Samuli Suomi <3490745+samulisuomi@users.noreply.github.com>
2022-09-13 16:44:01 +0300

    Improve the cache
    * Still not perfect, as when there are query params Chrome for Android
      complains: "To use Share to HN for the first time, please connect to
      the internet". Then again, HN doesn't work without online connectivity
      either so this should be fine.

--- serviceworker.js
+++ serviceworker.js
@@ -3,16 +3,16 @@ var staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll([
+        "."
+      ]);
     })
   );
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-
   event.respondWith(
-    caches.match(event.request).then(function (response) {
+    caches.match(event.request, { ignoreSearch: true }).then(function (response) {
       return response || fetch(event.request);
     })
   );
